f3756c72338ab05a764b29c4a89b1844f0a3ace6
평일반 <@ITBANK28-PC>
2015-07-10 15:20:54 +0900

    2015.07.10 -for loop 합계

--- JavaOperator/src/condition/ManOrWoman.java
+++ JavaOperator/src/condition/ManOrWoman.java
@@ -11,7 +11,7 @@ public class ManOrWoman {
 	public void ssn() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("주민번호를 입력하세요(예800101-2123456");
-		String ssn = scanner.next();
+		String ssn = scanner.next(),msg="";
 		/*
 		 charAt(n) 이 메소드는 0부터 시작하여 n번째 글자 하나만 출력하는데
 		 출력결과를 프로그래밍에서는 리턴값이라고 한다.
@@ -25,6 +25,7 @@ public class ManOrWoman {
 		 * 5번 외국인
 		 * 잘못 입력한 값입니다
 		 */
+		
 		switch (ch) {
 		case '1': case '3': msg="남자"; break;
 		case '2': case '4': msg="여자"; break;
